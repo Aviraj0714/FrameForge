c8efa6fa5d013b14082504acd645ebfb046860fe
Aviraj Bhaliya <131906596+Aviraj0714@users.noreply.github.com>
2025-03-22 01:38:33 +0530

    Update AppHeader.tsx

--- app/_components/AppHeader.tsx
+++ app/_components/AppHeader.tsx
@@ -10,7 +10,7 @@ function AppHeader({ hideSidebar = false }) {
                 <div className='flex items-center gap-2'>
                     <Image src={'/logo.svg'} alt='logo' width={100} height={100}
                         className='w-[40px] h-[40px]' />
-                    <h2 className='font-bold text-lg'>Wireframe to Code</h2>
+                    <h2 className='font-bold text-lg'>FrameForge</h2>
                 </div>
             }
             <ProfileAvatar />
